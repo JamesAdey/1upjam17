586f9d9db28a989625eda505c0a55598bb3558c6
James <jamesadey91@gmail.com>
2017-06-10 02:05:41 +0100

    corn + field generator
    added corn models
    wip on random field generator

--- Assets/Scripts/WorldTile.cs
+++ Assets/Scripts/WorldTile.cs
@@ -5,6 +5,29 @@ using UnityEngine;
 [System.Serializable]
 public struct WorldTile
 {
+	private int _x;
+	private int _y;
+
+	public int x {
+		get {
+			return _x;
+		}
+	}
+
+	public int y {
+		get {
+			return _y;
+		}
+	}
+
+	public WorldTile (TileType t, int nx, int ny)
+	{
+		tileType = t;
+		plant = null;
+		_x = nx;
+		_y = ny;
+	}
+
 	public TileType tileType;
 	public PlantBase plant;
 
